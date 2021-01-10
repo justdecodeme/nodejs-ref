7d6ff03d65cfc2ae03bd98e189ce9d9d68d4b831
Rakesh <justdecodeme@hotmail.com>
2021-01-10 20:46:14 +0530

    almost about to finish

--- refrences/streams/child-process.js
+++ refrences/streams/child-process.js
@@ -1,4 +1,16 @@
 const cp = require("child_process");
 
 cp.exec("open http://www.linkedin.com/justdecodeme");
+
 cp.exec("open -a Terminal .");
+
+cp.exec("ls", (err, data) => {
+	if (err) {
+		throw err;
+	}
+	console.log(data);
+});
+
+cp.exec("node readStream", (err, data, stderr) => {
+	console.log(data);
+});
